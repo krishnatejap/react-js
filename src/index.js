92d7720e8cb4c6255fd1f232ef8f67f41142f575
Krishna Teja Panchalavarapu <krishnatejap@gmail.com>
2020-02-11 09:19:48 +0530

    tic-tac-toe component control

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square" onClick={() => this.props.onClick()}> {this.props.value}
+                {/*TODO*/}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+    }
+
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row-1">
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                </div>
+                <div className="board-row-2">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row-3">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
